5ba50d275339f60ac83cd1fc6e915521b2267a8a
aczech17 <adrian-czech@outlook.com>
2024-07-12 23:20:48 +0200

    testing in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use crate::compress::compress;
+
 mod huffman_tree;
 mod file_reader;
 mod bit_vector;
+mod compress;
+mod file_writer;
 
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let input_filename = "empty";
+    let output_filename = "test.test2";
+
+    compress(input_filename, output_filename)
+        .unwrap();
 }
