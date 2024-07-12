98afbd3e8a1ec08b5c1d9b3f70648a85ed32cca6
aczech17 <adrian-czech@outlook.com>
2024-07-12 23:18:49 +0200

    few features for bit vector

--- src/bit_vector.rs
+++ src/bit_vector.rs
@@ -1,5 +1,6 @@
 pub type Bit = u8;
 
+#[derive(Clone)]
 pub struct BitVector
 {
     data: Vec<u8>,
@@ -40,8 +41,13 @@ impl BitVector
         }
     }
 
-    pub fn pop(&mut self)
+    pub fn pop_bit(&mut self)
     {
+        if self.bits_count == 0
+        {
+            panic!("Popping bit from an empty vector");
+        }
+
         let byte_number = (self.bits_count - 1) / 8;
         let bit_number = (self.bits_count - 1) % 8;
 
@@ -56,11 +62,27 @@ impl BitVector
         }
     }
 
-    pub fn get(&self, index: usize) -> Bit
+    pub fn get_bit(&self, index: usize) -> Bit
     {
         let byte_number = index / 8;
         let bit_number = index % 8;
 
         (self.data[byte_number] >> (7 - bit_number)) & 1
+    }
+
+    pub fn get_data(&self) -> &Vec<u8>
+    {
+        &self.data
+    }
+
+    pub fn clear(&mut self)
+    {
+        self.data.clear();
+        self.bits_count = 0;
+    }
+
+    pub fn size(&self) -> usize
+    {
+        self.bits_count
     }
 }
