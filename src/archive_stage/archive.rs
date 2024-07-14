982e2938e0ea7b8e91f99d25d5efb7f3d7d1c0cd
aczech17 <adrian-czech@outlook.com>
2024-07-15 01:57:25 +0200

    roboczy commit 3 (comp, decomp działa)

--- src/archive_stage/archive.rs
+++ src/archive_stage/archive.rs
@@ -3,22 +3,22 @@ use crate::io_utils::byte_writer::ByteWriter;
 
 pub fn archive(input_paths: Vec<String>, output_filename: String)
 {
-    let mut output_writer = ByteWriter::new(&output_filename)
-        .unwrap();
-    let mut all_paths: Vec<&String> = vec![];
-
-    for input_path in input_paths
-    {
-        let directory_info = DirectoryInfo::new(&input_path);
-        let mut directory_paths = directory_info.get_all_file_paths();
-        all_paths.append(&mut directory_paths);
-
-        let directory_info = serde_json::to_string(&directory_info)
-            .unwrap();
-        directory_info.into_bytes().iter().for_each(|&byte| output_writer.write_byte(byte));
-        output_writer.write_byte(0);
-    }
-    output_writer.write_byte(1);
+    // let mut output_writer = ByteWriter::new(&output_filename)
+    //     .unwrap();
+    // let mut all_paths: Vec<&String> = vec![];
+    //
+    // for input_path in input_paths
+    // {
+    //     let directory_info = DirectoryInfo::new(&input_path);
+    //     let mut directory_paths = directory_info.get_all_file_paths();
+    //     all_paths.append(&mut directory_paths);
+    //
+    //     let directory_info = serde_json::to_string(&directory_info)
+    //         .unwrap();
+    //     directory_info.into_bytes().iter().for_each(|&byte| output_writer.write_byte(byte));
+    //     output_writer.write_byte(0);
+    // }
+    // output_writer.write_byte(1);
 
 
 }
