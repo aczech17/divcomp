a74f7d62d2f7c5d4c85d8e440e6231087f6ff3d9
aczech17 <adrian-czech@outlook.com>
2024-07-14 13:58:57 +0200

    added byte buffer

---
+++ src/byte_buffer.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::Write;
+
+const BUFFER_SIZE: usize = 1024;
+
+pub struct ByteBuffer
+{
+    file_handle: File,
+    buffer: [u8; BUFFER_SIZE],
+    bytes_in_buffer: usize,
+}
+
+impl ByteBuffer
+{
+    pub fn new(output_filename: &str) -> Result<ByteBuffer, String>
+    {
+        let file_handle = File::create(output_filename)
+            .map_err(|_| format!("Could not create file buffer for {}.", output_filename))?;
+
+
+        let byte_buffer = ByteBuffer
+        {
+            file_handle,
+            buffer: [0; BUFFER_SIZE],
+            bytes_in_buffer: 0,
+        };
+
+        Ok(byte_buffer)
+    }
+
+    fn flush(&mut self)
+    {
+        if self.bytes_in_buffer > 0
+        {
+            self.file_handle.write_all(&self.buffer[0..self.bytes_in_buffer])
+                .unwrap();
+        }
+        self.bytes_in_buffer = 0;
+    }
+
+    pub fn write_byte(&mut self, byte: u8)
+    {
+        if self.bytes_in_buffer == BUFFER_SIZE
+        {
+            self.flush();
+        }
+
+        self.buffer[self.bytes_in_buffer] = byte;
+        self.bytes_in_buffer += 1;
+    }
+}
+
+impl Drop for ByteBuffer
+{
+    fn drop(&mut self)
+    {
+        self.flush();
+    }
+}
