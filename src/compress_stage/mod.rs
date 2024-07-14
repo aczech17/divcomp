a203596db629619253e217e25c7819dc3d758f2a
aczech17 <adrian-czech@outlook.com>
2024-07-14 16:10:07 +0200

    refactor boży

--- src/compress_stage/mod.rs
+++ src/compress_stage/mod.rs
@@ -1,7 +1,8 @@
-pub mod bit_vector;
-pub mod file_writer;
-pub mod huffman_tree;
-pub mod compress;
-pub mod file_reader;
-pub mod decompress;
+mod bit_vector;
+mod file_reader;
+mod file_writer;
 mod byte_writer;
+mod huffman_tree;
+pub mod compress;
+
+pub mod decompress;
