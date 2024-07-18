6c4666065e2e71d1ab2e4742bbf2372a2459a3c2
aczech17 <adrian-czech@outlook.com>
2024-07-18 04:04:47 +0200

    DRY

--- src/compress_stage/decompress.rs
+++ src/compress_stage/decompress.rs
@@ -64,70 +64,83 @@ impl Decompressor
     }
 
 
-    pub fn decompress_bytes_to_memory(&mut self, bytes_to_get: usize)
-        -> Result<Vec<u8>, DecompressError>
+    fn decompress_somewhere
+    (
+        &mut self,
+        bytes_count: usize,
+        output_filename: Option<String>,
+        save_to_memory: bool
+    )
+        -> Result<Option<Vec<u8>>, DecompressError>
     {
-        let mut bytes = Vec::with_capacity(bytes_to_get);
+
+        let mut bytes_decompressed = 0;
+        let mut potential_result_vector: Option<Vec<u8>> = match save_to_memory
+        {
+            true => Some(Vec::with_capacity(bytes_count)),
+            false => None,
+        };
+
+        let mut potential_file_writer = match output_filename
+        {
+            Some(filename) =>
+            {
+                let writer = ByteWriter::new(&filename)
+                    .map_err(|_| DecompressError::Other)?;
+
+                Some(writer)
+            }
+
+            None => None,
+        };
 
         let mut potential_codeword = BitVector::new();
-        while bytes.len() < bytes_to_get
+        while bytes_decompressed < bytes_count
         {
             let bit = self.file_reader.read_bit()
                 .ok_or(DecompressError::FileTooShort)?;
-
             potential_codeword.push_bit(bit);
+
             if let Some(byte) = self.get_byte_from_codeword(&potential_codeword)
             {
-                bytes.push(byte);
+                if let Some(vector) = &mut potential_result_vector
+                {
+                    vector.push(byte);
+                }
+
+                if let Some(writer) = &mut potential_file_writer
+                {
+                    writer.write_byte(byte);
+                }
+
+                bytes_decompressed += 1;
                 potential_codeword.clear();
             }
         }
 
-        Ok(bytes)
+        Ok(potential_result_vector)
+    }
+
+    pub fn decompress_bytes_to_memory(&mut self, bytes_to_get: usize)
+        -> Result<Vec<u8>, DecompressError>
+    {
+        let bytes =
+            self.decompress_somewhere(bytes_to_get, None, true)?;
+
+        Ok(bytes.unwrap())
     }
 
     pub fn decompress_bytes_to_file(&mut self, output_filename: &str, count: usize)
         -> Result<(), DecompressError>
     {
-        let mut bytes_decompressed = 0;
-
-        let mut output_writer = ByteWriter::new(output_filename)
-            .map_err(|_| DecompressError::Other)?;
-
-        let mut potential_codeword = BitVector::new();
-        while bytes_decompressed < count
-        {
-            let bit = self.file_reader.read_bit()
-                .ok_or(DecompressError::FileTooShort)?;
-
-            potential_codeword.push_bit(bit);
-            if let Some(byte) = self.get_byte_from_codeword(&potential_codeword)
-            {
-                output_writer.write_byte(byte);
-                bytes_decompressed += 1;
-                potential_codeword.clear();
-            }
-        }
+        self.decompress_somewhere(count, Some(output_filename.to_owned()), false)?;
 
         Ok(())
     }
 
     pub fn ignore(&mut self, bytes_count: usize) -> Result<(), DecompressError>
     {
-        let mut bytes_decompressed = 0;
-        let mut potential_codeword = BitVector::new();
-        while bytes_decompressed < bytes_count
-        {
-            let bit = self.file_reader.read_bit()
-                .ok_or(DecompressError::FileTooShort)?;
-
-            potential_codeword.push_bit(bit);
-            if let Some(_byte) = self.get_byte_from_codeword(&potential_codeword)
-            {
-                bytes_decompressed += 1;
-                potential_codeword.clear();
-            }
-        }
+        self.decompress_somewhere(bytes_count, None, false)?;
 
         Ok(())
     }
